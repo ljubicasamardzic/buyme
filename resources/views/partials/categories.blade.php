f3a905de4f28f5dc257896b7bac754a02c9e5cf1
ljubicasamardzic <ljubica.samardzic22@gmail.com>
2022-04-11 21:22:42 +0200

    zoom in effect on categories

--- resources/views/partials/categories.blade.php
+++ resources/views/partials/categories.blade.php
@@ -1,15 +1,21 @@
 <div class="container-fluid">
     <div class="row mt-4 d-flex justify-content-center">
         @foreach($categories as $category)
-            <div class="col-12 col-md-5 pb-1">
-                <a href="{{ route('categories.show', ['category' => $category]) }}"
-                   class="cat-img position-relative overflow-hidden mb-3 gold"
-                >
-                    <div class="cat-item d-flex flex-column border mb-4">
-                            <img class="img-fluid" src="{{ $category->path }}" alt="">
-                        <h5 class="font-weight-semi-bold m-0 text-secondary">{{ $category->name }}</h5>
-                    </div>
-                </a>
+            <div class="col-12 col-md-5 pb-1 mb-3">
+                <div class="cat-item">
+                    <a href="{{ route('categories.show', ['category' => $category]) }}"
+                       class="cat-img position-relative mb-3 gold"
+                    >
+                        <div class="card">
+                            <div class="card-header p-0 overflow-hidden">
+                                <img class="img-fluid" src="{{ $category->path }}" alt="">
+                            </div>
+                           <div class="card-footer" style="background-color: #fff!important;">
+                               <h5 class="font-weight-semi-bold m-0 text-secondary">{{ $category->name }}</h5>
+                           </div>
+                        </div>
+                    </a>
+                </div>
             </div>
         @endforeach
     </div>
